e3c71b33cb62b2065d815cd7ef0cf84c42813116
Raj <07umarjan@gmail.com>
2019-06-23 21:54:12 -0400

    Added Call Service

---
+++ src/CallRecorder.Main/CallService/CallRecorder/CallRecorder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CallService
+{
+    public class CallRecorder : ICallRecorder
+    {
+        public Task<bool> PauseRecording()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ResumeRecording()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> StartRecording()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> StopRecording()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/CallRecorder.Main/CallService/CallRecorder/ICallRecorder.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace CallService
+{
+    public interface ICallRecorder
+    {
+        Task<bool> StartRecording();
+        Task<bool> StopRecording();
+        Task<bool> PauseRecording();
+        Task<bool> ResumeRecording();
+    }
+}
